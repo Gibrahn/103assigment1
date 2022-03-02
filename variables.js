04bb6ee5dfe8cc374285f5ea30e2650778d331e1
Gibrahn <gibrahn.duarte18@tectijuana.edu.mx>
2022-03-01 18:13:15 -0800

    First Version
    adding html and varibale.js

---
+++ variables.js
@@ -0,0 +1,21 @@
+let name = "Gibrahn";
+let lastName = "Duarte"
+let age = 33;
+let jersey = 9;
+let teamName = "Real Madrid";
+let rivalTeam = "Barcelona";
+let position = "Goalkeeper";
+let favoriteFood = "Chinese";
+let salary = 500000;
+let favoriteAnimal = "wolf";
+let favoriteColor = "Blue";
+let matches = 20;
+let goals = 15;
+let assists = 7;
+let yellowCards = 3;
+let redCards = 1;
+let date = "March 1st";
+let gameTime = "3:00 PT";
+
+ let template = `my name is ${name} ${lastName}, my age is ${age}, my jersey is${jersey}`
+document.write(template);
